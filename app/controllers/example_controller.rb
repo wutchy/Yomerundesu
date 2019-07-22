56108a40c8664a5878f63dc02eaa8a088a41f60b
shumaikunkun <s1711520@gmail.com>
2019-07-22 18:43:06 +0900

    データ整形

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -6,28 +6,20 @@ class ExampleController < ApplicationController
 
   def aaa
 
-
-
-
-
     visual_recognition = VisualRecognitionV3.new(
       version: "2018-03-19",
       iam_apikey: "UijvdSiykSyVcX9zu4yyOch4dzLYgDvMj8bVJZfo8ERL"
     )
 
-
-
-    File.open("#{Rails.root}/public/banana.gif") do |images_file|
+    @image="/banana.gif"
+    File.open("#{Rails.root}/public#{@image}") do |images_file|
       classes = visual_recognition.classify(
         images_file: images_file,
-        # threshold: "0.2",
-        # owners: ["me"]
         classifier_ids:["food"]
       )
-      @aaa= JSON.pretty_generate(classes.result)
+      @aaa= JSON.parse(JSON.pretty_generate(classes.result))["images"][0]["classifiers"][0]["classes"]
+
+
     end
-
   end
-
-
 end
