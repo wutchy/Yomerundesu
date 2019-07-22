fc6eb34390d005005d7b76a20c87ff939df4114b
shumaikunkun <s1711520@gmail.com>
2019-07-23 00:29:28 +0900

    顔認識に成功

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -5,12 +5,10 @@ class ExampleController < ApplicationController
   include IBMWatson
 
   def aaa
-
     visual_recognition = VisualRecognitionV3.new(
       version: "2018-03-19",
       iam_apikey: "UijvdSiykSyVcX9zu4yyOch4dzLYgDvMj8bVJZfo8ERL"
     )
-
     @image="/banana.gif"
     File.open("#{Rails.root}/public#{@image}") do |images_file|
       classes = visual_recognition.classify(
@@ -18,8 +16,37 @@ class ExampleController < ApplicationController
         classifier_ids:["food"]
       )
       @aaa= JSON.parse(JSON.pretty_generate(classes.result))["images"][0]["classifiers"][0]["classes"]
-
-
     end
   end
+
+  def bbb
+    visual_recognition = VisualRecognitionV3.new(
+      version: "2018-03-19",
+      iam_apikey: "UijvdSiykSyVcX9zu4yyOch4dzLYgDvMj8bVJZfo8ERL"
+    )
+    @image="/english.jpg"
+    File.open("#{Rails.root}/public#{@image}") do |images_file|
+      classes = visual_recognition.classify(
+        images_file: images_file,
+        #classifier_ids:["food"]
+        accept_language: ["en", "ja"]
+      )
+      @bbb=JSON.pretty_generate(classes.result)
+    end
+  end
+
+  def ccc
+    visual_recognition = VisualRecognitionV3.new(
+      version: "2018-03-19",
+      iam_apikey: "UijvdSiykSyVcX9zu4yyOch4dzLYgDvMj8bVJZfo8ERL"
+    )
+    @image="/shuma.jpg"
+    File.open("#{Rails.root}/public#{@image}") do |images_file|
+      faces = visual_recognition.detect_faces(
+        images_file: images_file
+      )
+      @ccc=JSON.pretty_generate(faces.result)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'example#aaa'
-
-
+  get 'example/bbb'
+  get 'example/ccc'
 end
