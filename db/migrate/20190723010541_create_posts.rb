98e189420da84e821a802436def6b61c64a40187
wutchy <dp4dp7@outlook.jp>
2019-07-26 02:15:58 +0900

    modify db/migrate/20190723010541_create_posts.rb

--- db/migrate/20190723010541_create_posts.rb
+++ db/migrate/20190723010541_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :image
-      t.striing :name
+      t.string :name
 
       t.timestamps
     end
