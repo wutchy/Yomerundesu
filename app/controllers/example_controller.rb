768eb41837b239535e37e80d130b9cdc0113886b
shumaikunkun <s1711520@gmail.com>
2019-07-25 18:57:50 +0900

    送信画像のディレクトリ変更

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -20,10 +20,10 @@ class ExampleController < ApplicationController
   def aaa
     visual_recognition = VisualRecognitionV3.new(
       version: "2018-03-19",
-      iam_apikey: ENV['APIKEY']
+      iam_apikey: "UijvdSiykSyVcX9zu4yyOch4dzLYgDvMj8bVJZfo8ERL"
     )
-    File.binwrite("public/test.jpg",params[:image].read)
-    @image="/test.jpg"
+    File.binwrite("public/images/test.jpg",params[:image].read)
+    @image="/images/test.jpg"
     File.open("#{Rails.root}/public#{@image}") do |images_file|
       classes = visual_recognition.classify(
         images_file: images_file,
@@ -38,10 +38,10 @@ class ExampleController < ApplicationController
   def bbb
     visual_recognition = VisualRecognitionV3.new(
       version: "2018-03-19",
-      iam_apikey: ENV['APIKEY']
+      iam_apikey: "UijvdSiykSyVcX9zu4yyOch4dzLYgDvMj8bVJZfo8ERL"
     )
-    File.binwrite("public/test.jpg",params[:image].read)
-    @image="/test.jpg"
+    File.binwrite("public/images/test.jpg",params[:image].read)
+    @image="/images/test.jpg"
     File.open("#{Rails.root}/public#{@image}") do |images_file|
       classes = visual_recognition.classify(
         images_file: images_file,
@@ -56,10 +56,10 @@ class ExampleController < ApplicationController
   def ccc
     visual_recognition = VisualRecognitionV3.new(
       version: "2018-03-19",
-      iam_apikey: ENV['APIKEY']
+      iam_apikey: "UijvdSiykSyVcX9zu4yyOch4dzLYgDvMj8bVJZfo8ERL"
     )
-    File.binwrite("public/test.jpg",params[:image].read)
-    @image="/test.jpg"
+    File.binwrite("public/images/test.jpg",params[:image].read)
+    @image="/images/test.jpg"
     File.open("#{Rails.root}/public#{@image}") do |images_file|
       faces = visual_recognition.detect_faces(
         images_file: images_file
