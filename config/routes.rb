24598a19c20b5c1f0039680dbb24047a845267d9
wutchy <dp4dp7@outlook.jp>
2019-07-27 11:41:32 +0900

    change rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'example/top'
+  root 'example#top'
   post 'example/upload'
   post 'example/aaa'
   post 'example/bbb'
