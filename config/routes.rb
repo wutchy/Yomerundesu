b5a26c29baf57b9284fcb771944feb0fe97a3a58
wutchy <dp4dp7@outlook.jp>
2019-07-28 13:56:45 +0900

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'example#top'
-  post 'example/upload'
-  post 'example/aaa'
-  post 'example/bbb'
-  post 'example/ccc'
-  post 'example/ddd'
+  root to: 'example#top'
+  match 'example/upload',to: 'example#upload', via: [:get,:post]
+  match 'example/aaa',to: 'example#aaa', via: [:get,:post]
+  match 'example/bbb',to: 'example#bbb', via: [:get,:post]
+  match 'example/ccc',to: 'example#ccc', via: [:get,:post]
+  match 'example/ddd',to: 'example#ddd', via: [:get,:post]
 
 
 
